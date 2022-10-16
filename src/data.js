486c1cf81f5bc83ad88d56a70d9e92d0b0d9379c
Lakshmii-R <lakshmi.r.242001@gmail.com>
2022-10-16 16:36:37 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    
+    image: "./project-1.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    
+    image: "./project-2.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+  {
+    title: "Chat",
+    subtitle: "React and Firebase",
+    
+    image: "./project-3.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+  {
+    title: " Todo App",
+    subtitle: "React Hooks",
+    
+    image: "./project-4.gif",
+    link: "https://github.com/Lakshmii-R",
+  },
+];
+
+
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "C",
+  "Node",
+  
+  
+];
