7acf8f93b4a09f33234126d85fe4be744d80ed37
Lakshmii-R <lakshmi.r.242001@gmail.com>
2022-10-16 20:59:16 +0530

    first commit

--- src/data.js
+++ src/data.js
@@ -1,28 +1,25 @@
 export const projects = [
-  {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    
-    image: "./project-1.gif",
-    link: "https://github.com/Lakshmii-R",
-  },
+  
   {
     title: "React Tracks",
     subtitle: "React and Python",
-    
+    description: 
+    "Explore declarative tracking a using react-trackling",
     image: "./project-2.gif",
     link: "https://github.com/Lakshmii-R",
   },
   {
     title: "Chat",
     subtitle: "React and Firebase",
-    
+    description:
+     "A real time chat application that supports many users to chat simultaneously",
     image: "./project-3.gif",
     link: "https://github.com/Lakshmii-R",
   },
   {
     title: " Todo App",
     subtitle: "React Hooks",
+    description: "A simple Todo app using react to add and delete items",
     
     image: "./project-4.gif",
     link: "https://github.com/Lakshmii-R",
